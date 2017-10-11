94cd72c5293a0ac724802af04219cbff2354ed57
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-10-11 20:34:01 +0100

    url filtering again

--- handlers/filter-target.js
+++ handlers/filter-target.js
@@ -2,19 +2,24 @@ const config = require('../config');
 const {URL} = require('url');
 
 module.exports = (req, res, next) => {
+    const url = req.query.url.toLowerCase();
+
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+        console.log(`Invalid URL - http(s) prefix missing: ${url}`);
+        return res.status(400).end(`Invalid URL - http(s) prefix missing: ${url}`);
+    }
+
     try {
-        let hostname = new URL(req.query.url).hostname;
+        let hostname = new URL(url).hostname;
 
         if (!hostname) {
-            console.log(`Failed to parse target ${req.query.url}`);
-            return res.status(400).end(`Failed to parse target ${req.query.url}`);
+            console.log(`Failed to parse target ${url}`);
+            return res.status(400).end(`Failed to parse target ${url}`);
         }
 
-        hostname = hostname.toLowerCase();
-
         if (!config.whitelist.targets.includes(hostname)) {
-            console.log(`Target ${req.query.url} not allowed`);
-            return res.status(403).end(`Target ${req.query.url} not allowed`);
+            console.log(`Target ${url} not allowed`);
+            return res.status(403).end(`Target ${url} not allowed`);
         }
 
         setImmediate(next);
