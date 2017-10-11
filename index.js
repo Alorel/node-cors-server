b2bceba54569643753c15eeb25b78f9898464269
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-10-11 20:23:32 +0100

    test throng

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const express = require('express');
-const app = express();
-const request = require('request');
+const throng = require('throng');
+const numCPUs = require('os').cpus().length;
 
-app.set('port', (process.env.PORT || 5000));
+console.log(`Starting ${numCPUs} workers...`);
 
-app.get('/', require('./handlers/set-headers'));
-app.get('/', require('./handlers/filter-origin'));
-app.get('/', require('./handlers/filter-querystring'));
-app.get('/', require('./handlers/filter-target'));
+throng(require('os').cpus().length, id => {
+  console.log(`Starting worker ${id}`);
 
-app.get('/', (req, res) => {
-  req.pipe(request(req.query.url))
-      .pipe(res);
-});
+  const express = require('express');
+  const app = express();
+  const request = require('request');
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  app.set('port', (process.env.PORT || 5000));
+
+  app.get('/', require('./handlers/set-headers'));
+  app.get('/', require('./handlers/filter-origin'));
+  app.get('/', require('./handlers/filter-querystring'));
+  app.get('/', require('./handlers/filter-target'));
+
+  app.get('/', (req, res) => {
+      req.pipe(request(req.query.url))
+          .pipe(res);
+  });
+
+  app.listen(app.get('port'), () => {
+      console.log(`Worker ${id} listening on port ${app.get('port')}`);
+  });
 });
