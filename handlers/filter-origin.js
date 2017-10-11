83f53f918e2df3c5bf2237de8686638e53ae41c6
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-10-11 19:59:55 +0100

    origin parsing fixed

--- handlers/filter-origin.js
+++ handlers/filter-origin.js
@@ -1,13 +1,29 @@
 const config = require('../config');
+const {URL} = require('url');
 
 module.exports = (req, res, next) => {
-    const origin = req.header('origin') || req.header('referer');
+    let origin = req.header('origin') || req.header('referer');
 
     if (!origin) {
-        res.status(400).end(`Could not determine origin`);
-    } else if (!config.whitelist.origins.includes(origin)) {
-        res.status(403).end(`Origin ${origin} not allowed.`);
-    } else {
+        return res.status(400).end(`Could not determine origin`);
+    }
+
+    try {
+        let hostname = new URL(origin).hostname;
+
+        if (!hostname) {
+            return res.status(400).end(`Failed to parse origin ${origin}`);
+        }
+
+        hostname = hostname.toLowerCase();
+
+        if (!config.whitelist.origins.includes(hostname)) {
+            return res.status(403).end(`Origin ${origin} not allowed.`);
+        }
+
         setImmediate(next);
+    } catch(e) {
+        console.error(e);
+        res.status(503).end();
     }
 };
